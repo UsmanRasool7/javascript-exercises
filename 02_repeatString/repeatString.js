8356a7833bd975747af87c6e9fc323265960a227
UsmanRasool7 <usmanrasool717@gmail.com>
2024-07-02 16:10:48 +0500

    repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+  let str = [];
+  for(let i=0;i<num;i++)
+  {
+    str.push(string);
+  }
 };
 
 // Do not edit below this line
